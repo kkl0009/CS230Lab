1beedf1a96979b0eb23b481273de6968b61002d0
Kollin Labowski <kkl0009@mix.wvu.edu>
2020-09-25 16:22:15 -0400

    Lab4 Completed

---
+++ about.php
@@ -0,0 +1,71 @@
+<?php
+require 'includes/header.php'
+?>
+<main>
+<link rel="stylesheet" href="css/about.css">
+        <div class="about">
+        <h1>The Galactic Republic</h1>
+        <h2>Based in Coruscant</h2>
+        <p>The Galactic Republic, headed by Supreme Chancellor Palpatine, provides the best security and stability to the known galaxy. The Galactic Republic includes the Senate, consisting entirely of Chancellor Palpatine, and is closely tied to the Jedi Council, which has such legendary memes as Mace Windu, Anakin Skywalker, and of course, Obi-Wan Kenobi</p>
+        </div>
+        <div class="link">
+            <a href="https://github.com/kkl0009/CS230Lab">https://github.com/kkl0009/CS230Lab</a>
+        </div>
+        <div class="row">
+            <div class="column">
+                <div class="card">
+                    <img src="images/palpatine.png">
+                    <div class="container">
+                        <h2>Chancellor Palpatine</h2>
+                        <div class="quote">
+                            <p>"I am the Senate"</p>
+                        </div>
+                    </div>              
+                </div>
+            </div>
+            <div class="column">
+                <div class="card">
+                    <img src="images/obi.png">
+                    <div class="container">
+                        <h2>Jedi Master Obi-Wan Kenobi</h2>
+                        <div class="quote">
+                            <p>"Hello there!"</p>
+                        </div>
+                    </div>              
+                </div>
+            </div>
+            <div class="column">
+                <div class="card">
+                    <img src="images/anakin.webp">
+                    <div class="container">
+                        <h2>Jedi Knight Anakin Skywalker</h2>
+                        <div class="quote">
+                            <p>"This is where the fun begins!"</p>
+                        </div>                   
+                    </div>              
+                </div>
+            </div>
+            <div class="column">
+                <div class="card">
+                    <img src="images/youngling.png">
+                    <div class="container">
+                        <h2>That One Random Youngling</h2>
+                        <div class="quote">
+                            <p>"WhAt ArE wE gOiNg To dO?"</p>
+                        </div>
+                    </div>              
+                </div>
+            </div>
+            <div class="column">
+                <div class="card">
+                    <img src="images/binks.webp">
+                    <div class="container">
+                        <h2>Naboo Senator Jar Jar Binks</h2>
+                        <div class="quote">
+                            <p>Totally not a Sith ;)</p>
+                        </div>
+                    </div>              
+                </div>
+            </div>
+        </div>
+</main>
